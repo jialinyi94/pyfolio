d61ebb6004022331755fa4b7125e4091c2356b1e
Jialin Yi <jialinyi@sas.upenn.edu>
2025-03-10 23:57:27 +0000

    replace M by ME

--- pyfolio/plotting.py
+++ pyfolio/plotting.py
@@ -1369,7 +1369,7 @@ def plot_turnover(returns, transactions, positions, turnover_denom='AGB',
     ax.yaxis.set_major_formatter(FuncFormatter(y_axis_formatter))
 
     df_turnover = txn.get_turnover(positions, transactions, turnover_denom)
-    df_turnover_by_month = df_turnover.resample("M").mean()
+    df_turnover_by_month = df_turnover.resample("ME").mean()
     df_turnover.plot(color='steelblue', alpha=1.0, lw=0.5, ax=ax, **kwargs)
     df_turnover_by_month.plot(
         color='orangered',
